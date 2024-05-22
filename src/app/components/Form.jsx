3d81ddd86ea1eb037edfbcaa0675aac7ff1a743b
takako-elitedigital <155658928+takako-elitedigital@users.noreply.github.com>
2024-05-22 13:10:17 -0700

    added form

---
+++ src/app/components/Form.jsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useState } from "react";
+
+export default function Form() {
+  const [values, setValues] = useState([]);
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value !== '') {
+        setValues([...values, value]);
+        setValue('');
+    }
+  };
+
+  return (
+    <>
+      <div className="bg-white p-4 shadow-[0_0_10px_rgba(0,0,0,0.1)] rounded-md h-[95vh] flex flex-col">
+        <form onSubmit={handleSubmit} className="mb-8">
+          <input className="w-full text-[18px] border-b-[1px] border-[#ccc] p-2 mb-4 text-center" type="text" placeholder="Type what's important for you." value={value} onChange={e => setValue(e.target.value)} />
+          <button className="text-center block w-[100px] mx-auto rounded-md py-1 border-[1px] border-[#ccc] " type="submit">Add</button>
+        </form>
+
+        <ul className="flex gap-3 mb-[80px]">
+            {values.map((value, index) => (
+                <li key={index} className="text-[14px] font-[400] italic">{value}</li>
+            ))}
+        </ul>
+
+        <button className="text-center block w-full mx-auto bg-[#6895e4] text-[24px] font-[600] rounded-md p-2 text-white">Make a decision</button>
+      </div>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import Form from "./components/Form";
 
 export default function Home() {
+
   return (
-    <main className="p-10">
-      <div className="p-4 shadow-[0_0_20px_rgba(0,0,0,0.1)] rounded-md">
-        Test
-      </div>
+    <main className="p-5">
+      <Form />
     </main>
   );
 }
