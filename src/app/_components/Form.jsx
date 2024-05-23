2a963eb46789ba404d5d15ba451e655256db5dc1
takako <155658928+takako-elitedigital@users.noreply.github.com>
2024-05-22 19:25:52 -0700

    fix for font size

--- src/app/_components/Form.jsx
+++ src/app/_components/Form.jsx
@@ -19,10 +19,10 @@ export default function Form() {
   return (
     <>
       <div className="wrapper">
-        <p className="text-center text-[14px] mb-[40px]">Add 3 or more important things for you.</p>
+        <p className="text-center mb-[40px]">Add 3 or more important things for you.</p>
         <form onSubmit={handleSubmit} className="mb-8">
           <input className="w-full text-[18px] border-b-[1px] border-[#ccc] p-2 mb-4 text-center" type="text" value={value} onChange={e => setValue(e.target.value)} />
-          <button className="text-center block w-[100px] mx-auto rounded-md py-1 border-[1px] border-[#ccc] " type="submit">Add</button>
+          <button className="text-center block w-[100px] mx-auto rounded-md py-1 border-[1px] border-[#ccc]" type="submit">Add</button>
         </form>
 
         <ul className="flex gap-x-3 gap-y-2 mb-[80px] w-full flex-wrap">
